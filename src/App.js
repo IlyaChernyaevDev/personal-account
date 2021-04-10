c9a4c01e1f5f5c186ac94f2800e316d5988f75f6
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-04-10 21:10:45 +0300

    Added Contacts component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import { Login } from './componetns/Login/';
+import { Contacts } from './componetns/Contacts/';
 
 function App() {
+  const [user, setUser] = useState({ login: false });
+  const [formData, setFormData] = useState({});
+
+  const handaleChangeInputs = (value, propertyName) => {
+    setFormData(prevState => {
+      return {
+        ...prevState,
+        [propertyName]: value
+      }
+    })
+  }
+
   return (
     <div className='App'>
+      {user.login ? <Contacts /> : <Login change={handaleChangeInputs} />}
       <Login />
     </div>
   );
